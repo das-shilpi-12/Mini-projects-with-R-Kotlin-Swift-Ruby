a00537c6cf3a32534e1c04b8c2d5f9c0851e365d
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-14 10:34:47 +0530

    Create Example.R

---
+++ Mini-projects-with-R-Programs/Data-Visualization/Bar-Plot/Example.R
@@ -0,0 +1,5 @@
+png(file = "shilpi.png")
+barplot(mtcars$hp, names.arg = rownames(mtcars)) 
+
+#a bar chart for the hp column of the mtcars dataset
+#rownames(mtcars) is used to select the row names of the data set.
