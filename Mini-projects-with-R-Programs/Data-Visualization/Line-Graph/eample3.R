e9be8705a5ec3ceaf44e1e0e64332e7345dd0b81
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-14 10:26:51 +0530

    Update eample3.R

--- Mini-projects-with-R-Programs/Data-Visualization/Line-Graph/eample3.R
+++ Mini-projects-with-R-Programs/Data-Visualization/Line-Graph/eample3.R
@@ -3,6 +3,7 @@ y1 <- c(1, 10)
 x2 <- c(1, 10)
 y2 <- c(10, 1)
 
+
 png(file = "shilpi.png")
-plot(x1, y1, type = "l")
-lines(x2, y2, type="l")
+plot(x1, y1, type = "l",col="green")
+lines(x2, y2, type="l",col="red")
